04aac651549e76dd2a5a4418769b89f2a2b1c969
KithomeJoseph <joseph.kithome@garisea.com>
2024-03-25 14:47:38 +0300

    "Mongo deploy link"

--- database/mongoConnector.ts
+++ database/mongoConnector.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 export const dbConection = ()=>{
     mongoose
-    .connect(`${process.env.MONGO_URI}`)
+    .connect('mongodb+srv://blackberry:6NRlEX0xkOj0lXEa@vibewire.uze1ulu.mongodb.net/?retryWrites=true&w=majority&appName=vibewire')
     .then(() => {
         console.log('Connected to MongoDB');
     
