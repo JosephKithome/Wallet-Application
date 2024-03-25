bb25ab22ecb0ff75b9a7c5341b668cbcba56f5ba
KithomeJoseph <joseph.kithome@garisea.com>
2024-03-25 13:06:22 +0300

    Defined environment variables

--- app.ts
+++ app.ts
@@ -660,7 +660,7 @@ app.get('/api/transaction/wallet/:walletId', async (req, resp) => {
 })
 
 // Server start
-const PORT: number = 3000;
+const PORT = process.env.PORT || 3000;
 
 function runServer() {
     try {
--- dist/app.js
+++ dist/app.js
@@ -570,7 +570,7 @@ exports.app.get('/api/transaction/wallet/:walletId', (req, resp) => __awaiter(vo
     }
 }));
 // Server start
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 function runServer() {
     try {
         (0, mongoConnector_1.dbConection)();
